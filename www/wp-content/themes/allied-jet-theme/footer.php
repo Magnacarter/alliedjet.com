3e2c53e672d50bbe781bfb9070d6891fe1cccc11
Adam.Carter <adamkristopher@gmail.com>
2015-07-01 05:26:51 -0500

    Code clean ups

--- www/wp-content/themes/allied-jet-theme/footer.php
+++ www/wp-content/themes/allied-jet-theme/footer.php
@@ -1,69 +1,85 @@
-			</div> <!--page content wrap from header.php-->
-			<footer>
+		</div> <!--page content wrap from header.php-->
 
-				<style type="text/css">
-					footer {
-						background-color: <?php the_field( 'footer_color', 'options' ) ?>;
-					}
-				</style>
+		<footer>
 
-				<section class="footer-content grid-container">
+			<style type="text/css">
 
-					<div class="aj-footer-menu grid-20 tablet-grid-100">
-						<?php
-							$defaults = array(
-								'theme_location'  => '',
-								'menu'            => 'footer-nav',
-								'container'       => 'div',
-								'container_class' => '',
-								'container_id'    => '',
-								'menu_class'      => 'menu',
-								'menu_id'         => '',
-								'echo'            => true,
-								'fallback_cb'     => 'wp_page_menu',
-								'before'          => '',
-								'after'           => '',
-								'link_before'     => '',
-								'link_after'      => '',
-								'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-								'depth'           => 0,
-								'walker'          => ''
-							);
-							wp_nav_menu( $defaults )
-						?>
-					</div>
+				footer {
 
-					<div class="footer-logo grid-55 tablet-grid-100">
-						<h4><a href="<?php echo esc_url( get_home_url() ) ?>"><img src="<?php the_field( 'nav_logo', 'options' ) ?>"/></a></h4>
-					</div>
+					background-color: <?php the_field( 'footer_color', 'options' ) ?>;
 
-					<div class="footer-contact grid-25 tablet-grid-100">
-						<a href="<?php echo esc_url( get_home_url() ) ?>/contact-us"><h3 class="footer-contact-us">Contact Us</h3>
+				}
 
-						<div class="contact-info-wrap">
+			</style>
 
-							<h3 class="footer-phone-number"><?php the_field( 'phone_number', 'options' ) ?></h3>
+			<section class="footer-content grid-container">
 
-							<ul class="footer-location-list">
-								<li class="footer-location-one"><?php the_field( 'header_location_1', 'options' ) ?></li>
+				<div class="aj-footer-menu grid-20 tablet-grid-100">
 
-								<li class="footer-location-two"><?php the_field( 'header_location_2', 'options' ) ?></li>
+					<?php
+						$defaults = array(
+							'theme_location'  => '',
+							'menu'            => 'footer-nav',
+							'container'       => 'div',
+							'container_class' => '',
+							'container_id'    => '',
+							'menu_class'      => 'menu',
+							'menu_id'         => '',
+							'echo'            => true,
+							'fallback_cb'     => 'wp_page_menu',
+							'before'          => '',
+							'after'           => '',
+							'link_before'     => '',
+							'link_after'      => '',
+							'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+							'depth'           => 0,
+							'walker'          => ''
+						);
+						wp_nav_menu( $defaults )
+					?>
 
-								<li class="footer-location-three"><?php the_field( 'header_location_3', 'options' ) ?></li>
-							</ul>
+				</div>
 
-						</div>
-					</div>
+				<div class="footer-logo grid-55 tablet-grid-100">
 
-				</section>
+					<h4><a href="<?php echo esc_url( get_home_url() ) ?>"><img src="<?php the_field( 'nav_logo', 'options' ) ?>"/></a></h4>
 
-				<div class="copyright-wrap grid-container">
-					<div class="copyright grid-100">
-						<h5>&copy;<?php echo date('Y ') ?><a href="<?php echo esc_url( get_home_url() ) ?>"><?php echo esc_html( get_bloginfo( 'name' ) ) ?></a></h5>
+				</div>
+
+				<div class="footer-contact grid-25 tablet-grid-100">
+
+					<a href="<?php echo esc_url( get_home_url() ) ?>/contact-us"><h3 class="footer-contact-us">Contact Us</h3>
+
+					<div class="contact-info-wrap">
+
+						<h3 class="footer-phone-number"><?php the_field( 'phone_number', 'options' ) ?></h3>
+
+						<ul class="footer-location-list">
+
+							<li class="footer-location-one"><?php the_field( 'header_location_1', 'options' ) ?></li>
+
+							<li class="footer-location-two"><?php the_field( 'header_location_2', 'options' ) ?></li>
+
+							<li class="footer-location-three"><?php the_field( 'header_location_3', 'options' ) ?></li>
+
+						</ul>
+
 					</div>
 				</div>
 
-			</footer>
+			</section>
+
+			<div class="copyright-wrap grid-container">
+
+				<div class="copyright grid-100">
+
+					<h5>&copy;<?php echo date('Y ') ?><a href="<?php echo esc_url( get_home_url() ) ?>"><?php echo esc_html( get_bloginfo( 'name' ) ) ?></a></h5>
+
+				</div>
+
+			</div>
+
+		</footer>
 
 		<?php wp_footer() ?>
 
--- www/wp-content/themes/allied-jet-theme/header.php
+++ www/wp-content/themes/allied-jet-theme/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
+
 	<head>
 		<meta charset="<?php echo esc_attr( get_bloginfo( 'charset' ) ) ?>">
 		<title><?php wp_title( '-', true, 'right' ); echo esc_html( get_bloginfo( 'name' ) ) ?></title>
@@ -8,39 +9,55 @@
 		<link rel="shortcut icon" href="<?php echo esc_url( get_stylesheet_directory_uri() ) ?>/favicon.ico" />
 		<?php wp_head() ?>
 	</head>
+
 	<body <?php body_class() ?>>
 
-	<div class="header-area-wrap grid-container">
-		<div class="header-logo grid-30 tablet-grid-33 mobile-grid-33">
-			<a href="<?php echo esc_url( get_home_url() ) ?>"><img src="<?php the_field( 'nav_logo', 'options' ) ?>"/></a>
+		<div class="header-area-wrap grid-container">
+
+			<div class="header-logo grid-30 tablet-grid-33 mobile-grid-33">
+
+				<a href="<?php echo esc_url( get_home_url() ) ?>"><img src="<?php the_field( 'nav_logo', 'options' ) ?>"/></a>
+
+			</div>
+
+			<nav class="main-nav grid-45 tablet-grid-33 mobile-grid-33">
+
+				<?php wp_nav_menu( array( 'menu' => 'main-nav' ) ) ?>
+
+			</nav>
+
+			<div class="header-locations grid-25 tablet-grid-33 mobile-grid-33">
+
+				<h3><?php the_field( 'call_today', 'options' ) ?></h3>
+
+				<h4>Locations</h4>
+
+				<ul class="location-list">
+
+					<li><?php the_field( 'header_location_1', 'options' ) ?></li>
+
+					<li><?php the_field( 'header_location_2', 'options' ) ?></li>
+
+					<li><?php the_field( 'header_location_3', 'options' ) ?></li>
+
+				</ul>
+
+			</div>
+
+			<div class="hamburger">
+
+				<a class="toggle-menu menu-left push-body"><img height="40" width="40" src="<?php the_field( 'hamburger_icon', 'options' ) ?>"></a></img>
+
+			</div>
+
+			<nav class="mobile-nav cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left">
+
+				<?php wp_nav_menu( array( 'menu' => 'push-nav' ) ) ?>
+
+			</nav>
+
 		</div>
 
-		<nav class="main-nav grid-45 tablet-grid-33 mobile-grid-33">
-			<?php wp_nav_menu( array( 'menu' => 'main-nav' ) ) ?>
-		</nav>
+		<div class="header-boarder"></div>
 
-		<div class="header-locations grid-25 tablet-grid-33 mobile-grid-33">
-			<h3><?php the_field( 'call_today', 'options' ) ?></h3>
-			<h4>Locations</h4>
-
-			<ul class="location-list">
-				<li><?php the_field( 'header_location_1', 'options' ) ?></li>
-
-				<li class="location-two"><?php the_field( 'header_location_2', 'options' ) ?></li>
-
-				<li class="location-three"><?php the_field( 'header_location_3', 'options' ) ?></li>
-			</ul>
-		</div>
-
-		<div class="hamburger">
-			<a class="toggle-menu menu-left push-body"><img height="40" width="40" src="<?php the_field( 'hamburger_icon', 'options' ) ?>"></a></img>
-		</div>
-
-		<nav class="mobile-nav cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left">
-			<?php wp_nav_menu( array( 'menu' => 'push-nav' ) ) ?>
-		</nav>
-	</div>
-
-	<div class="header-boarder"></div>
-
-	<div class="aj-page-content-wrap">
+		<div class="aj-page-content-wrap">
