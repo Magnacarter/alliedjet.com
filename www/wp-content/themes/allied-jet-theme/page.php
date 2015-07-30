3251237bcadf5538462cd4ff20626328d95cb5d0
Adam.Carter <adamkristopher@gmail.com>
2015-07-29 19:59:31 -0500

    More fixes to service page

--- www/wp-content/themes/allied-jet-theme/page.php
+++ www/wp-content/themes/allied-jet-theme/page.php
@@ -5,21 +5,17 @@
 get_header() ?>
 
 	<!-- Services Page -->
-	<?php if( is_page( 'services' ) ) : ?> 
+	<?php if ( is_page( 'service' ) ) : ?> 
 
 		<?php get_template_part( 'serve' ) ?>
 
-	<?php endif ?>
-
 	<!-- Our Company Page -->
-	<?php if ( is_page( 'our-company' ) ) : ?>
+	<?php elseif ( is_page( 'our-company' ) ) : ?>
 
 		<?php get_template_part( 'company' ) ?>
 
-	<?php endif ?>
-
 	<!-- Contact Page -->
-	<?php if ( is_page( 'contact-us' ) ) : ?>
+	<?php elseif ( is_page( 'contact-us' ) ) : ?>
 
 		<?php get_template_part( 'contact' ) ?>
 
